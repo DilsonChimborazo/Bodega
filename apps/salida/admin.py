a1461b02d3e923673a8e9a399b356590a1bdaa56
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-09-20 11:00:35 -0500

    trabajo completo

--- apps/salida/admin.py
+++ apps/salida/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.registro.models import Registro
+from apps.salida.models import Salida
 
 # Register your models here.
+admin.site.register(Registro)
+admin.site.register(Salida)
