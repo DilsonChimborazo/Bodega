064ae79168a9f18af94f272d790766d71bc20bf0
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-10-04 11:42:43 -0500

    cambio a modelviewset

--- apps/registro/api/router.py
+++ apps/registro/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from apps.registro.api.views import registroViewSet
+from apps.registro.api.views import SalidaModelViewSet
 
 router_registro = DefaultRouter()
-router_registro.register(prefix="registro", basename="registro", viewset=registroViewSet ) 
+router_registro.register(prefix="registro", basename="registro", viewset=SalidaModelViewSet ) 
--- apps/salida/api/router.py
+++ apps/salida/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from apps.salida.api.views import SalidaViewSet
+from apps.salida.api.views import SalidaModelViewSet
 
 router_salida = DefaultRouter()
-router_salida.register(prefix="salida", basename="salida", viewset=SalidaViewSet ) 
+router_salida.register(prefix="salida", basename="salida", viewset=SalidaModelViewSet ) 
