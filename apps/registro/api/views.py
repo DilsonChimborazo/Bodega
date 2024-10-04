455d6eb6f939ceb1c126effd70c3225315ad4c61
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-10-04 10:39:49 -0500

    update,partial_update,destroy aprendiendo a utilizar

--- apps/registro/api/views.py
+++ apps/registro/api/views.py
@@ -1,16 +1,47 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from apps.registro.models import Registro
 from apps.registro.api.serializers import registroSerializers
 
-class registroApiView(APIView):
-    def get(self, request):
+class registroViewSet(ViewSet):
+    def list (self, request):
         serializer = registroSerializers(Registro.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
     
-    def post(self,request):
+    def retrieve(self, request, pk=int):
+        serializer = registroSerializers(Registro.objects.get(pk=pk))
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    def create(self,request):
         serializer = registroSerializers(data=request.data)
         serializer.is_valid(raise_exception=True)#validacion de datos correctos
         serializer.save()
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    def update(self, request, pk): 
+        try:
+            serializer = Registro.objects.get(pk=pk)
+            serializer = registroSerializers(serializer, data=request.data)
+            serializer.is_valid(raise_exception=True) 
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data) 
+        except Registro.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"Registro no encontrado."})
+        
+    def partial_update(self, request, pk):
+        try:
+            serializer = Registro.objects.get(pk=pk)
+            serializer = registroSerializers(serializer, data=request.data)
+            serializer.is_valid(raise_exception=True) 
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data)
+        except Registro.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"Registro no encontrado."})
+
+        
+    def destroy (self, request, pk):
+        serializer = Registro.objects.get(pk=pk)
+        serializer.delete()
+        return Response(status=status.HTTP_200_OK,)
--- apps/salida/api/views.py
+++ apps/salida/api/views.py
@@ -1,16 +1,47 @@
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from apps.salida.models import Salida
 from apps.salida.api.serializers import SalidaSerializers
 
-class SalidaApiView(APIView):
-    def get(self, request):
+class SalidaViewSet(ViewSet):
+    def list(self, request):
         serializer = SalidaSerializers(Salida.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
     
-    def post(self,request):
+    def retrieve(self, request, pk=int):
+        serializer = SalidaSerializers(Salida.objects.get(pk=pk))
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    def create(self,request):
         serializer = SalidaSerializers(data=request.data)
         serializer.is_valid(raise_exception=True)#validacion de datos correctos
         serializer.save()
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+    def update(self, request, pk):
+        try:
+            serializer = Salida.objects.get(pk=pk)
+            serializer = SalidaSerializers(serializer, data=request.data)
+            serializer.is_valid(raise_exception=True) 
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data) 
+        except Salida.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"Registro no encontrado."})
+        
+    def partial_update(self, request, pk):
+        try:
+            serializer = Salida.objects.get(pk=pk)
+            serializer = SalidaSerializers(serializer, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True) 
+            serializer.save()
+            return Response(status=status.HTTP_200_OK, data=serializer.data)
+        except Salida.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND, data={"Registro no encontrado."})
+
+        
+    def destroy (self, request, pk):
+        serializer = Salida.objects.get(pk=pk)
+        serializer.delete()
+        return Response(status=status.HTTP_200_OK,)
